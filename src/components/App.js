7ec6299be4a143cd477103c8701eed6e19ede29e
suryarajrs <suryars@outlook.in>
2024-02-18 02:05:20 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,62 @@ import '../styles/App.css';
 const App = () => {
   // write your code here 
 
+  const [userInput, setuserInput]=useState(0)
+
+  const handleDownkey = (e)=>{
+
+    if (e.key === 'Enter'){
+      const input = Math.floor(parseFloat(e.target.value));
+
+      if(isNaN(input)||input<0){
+        setuserInput(0)
+        
+      }else{
+        setuserInput(input)
+      }
+    }
+
+
+
+
+  }
+
+  useEffect(()=>{
+
+    let intervelUniqueID;
+
+    if(userInput >0){
+
+      intervelUniqueID = setInterval(()=>{
+        setuserInput((prev)=>{
+          return prev-1
+        })
+      
+      },1000)
+  
+
+      
+    }
+
+     
+
+  return()=>{
+
+    clearInterval(intervelUniqueID)
+
+  }
+  },[userInput])
+
+
+
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" type="number"  onKeyDown={handleDownkey}/>sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{userInput}</div>
     </div>
   )
 }
